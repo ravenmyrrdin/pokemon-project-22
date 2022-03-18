e8cd0a964a68731158120a132d047454eb64f173
The_Raven <ravenvanhove@hotmail.be>
2022-03-18 10:39:09 +0100

    added page

--- src/net/views/ViewTemplates.ts
+++ src/net/views/ViewTemplates.ts
@@ -1,38 +1,37 @@
-import { NextFunction } from 'express';
-import { IViewTemplate } from '../interfaces/IViewTemplate';
-import { IWebResponse } from '../interfaces/IWebResponse';
+import { NextFunction } from "express";
+import { IViewTemplate } from "../interfaces/IViewTemplate";
+import { IWebResponse } from "../interfaces/IWebResponse";
 
 /**
  * Namespace containing IViewTemplates, used for registering views based on generic type attribute (T).
- * models must be made in IViewTemplates.ts otherwise they wont be detected, 
+ * models must be made in IViewTemplates.ts otherwise they wont be detected,
  * todo: look into universal solution..;
  */
 export namespace ViewTemplates {
-  type Wrapper < T > = {
-    new(...args: any[]): T;
+  type Wrapper<T> = {
+    new (...args: any[]): T;
 
     readonly prototype: T;
-  }
+  };
 
-  const viewImplementations: Wrapper < IViewTemplate > [] = [];
+  const viewImplementations: Wrapper<IViewTemplate>[] = [];
 
   /**
-   * Get all registered views using the set attribute 
-   * 
+   * Get all registered views using the set attribute
+   *
    * @returns IViewTemplateModel array
    */
-  export function getViews(): Wrapper < IViewTemplate > [] 
-  {
+  export function getViews(): Wrapper<IViewTemplate>[] {
     return viewImplementations;
   }
 
   /**
    * sets an interface object to the views template stack
-   * 
+   *
    * @param ctor extended type of IViewTemplateModel
    * @returns IViewTemplateModel wrapper
    */
-  export function set < T extends Wrapper < IViewTemplate >> (ctor: T) {
+  export function set<T extends Wrapper<IViewTemplate>>(ctor: T) {
     viewImplementations.push(ctor);
     return ctor;
   }
@@ -41,77 +40,96 @@ export namespace ViewTemplates {
 
 //#region Page objects
 /**
- * Implementation of IViewTemplate interface in class style. 
- * 
+ * Implementation of IViewTemplate interface in class style.
+ *
  */
 @ViewTemplates.set
 class Index {
-  get(req: Request, res: Response, next: NextFunction): IWebResponse  {
-    return {data:{}}
+  get(req: Request, res: Response, next: NextFunction): IWebResponse {
+    return { data: {} };
   }
 
   post = undefined;
 }
 /**
- * Implementation of IViewTemplate interface in class style. 
- * 
+ * Implementation of IViewTemplate interface in class style.
+ *
  */
 @ViewTemplates.set
 class GetExample {
   get(req: Request, res: Response, next: NextFunction): IWebResponse {
     const index: number = Number.parseInt(`${(req as any).query.index}`);
 
-    return {data: {
-      person: isNaN(index) ? "No person index defined" : [{name: "Jannick Oste"}, {name: "Tom Bom"}][index],
-      index: isNaN(index) ? -1 : index
-    }}
+    return {
+      data: {
+        person: isNaN(index)
+          ? "No person index defined"
+          : [{ name: "Jannick Oste" }, { name: "Tom Bom" }][index],
+        index: isNaN(index) ? -1 : index,
+      },
+    };
   }
 
   post = undefined;
-} 
+}
 
 export const postExample: IViewTemplate = {
   post: undefined,
-  get: (req: Request, res: Response, next: NextFunction) =>
-  {
+  get: (req: Request, res: Response, next: NextFunction) => {
     console.log("posted from new post");
     const response: IWebResponse = {
-      data:
-      {
-        message: undefined
-      }
-    }
+      data: {
+        message: undefined,
+      },
+    };
+
+    return response;
+  },
+};
+
+@ViewTemplates.set
+class PostExample {
+  get(req: Request, res: Response, next: NextFunction): IWebResponse {
+    const response: IWebResponse = {
+      data: {
+        message: undefined,
+      },
+    };
+
+    return response;
+  }
+
+  public post(req: any, res: any, next: NextFunction): IWebResponse {
+    const response: IWebResponse = {
+      data: {
+        message: `<h2>Welcome, ${(req.body as any).fname}</h2>`,
+      },
+    };
 
     return response;
   }
 }
 
 @ViewTemplates.set
-class PostExample
-{
-  get(req: Request, res: Response, next: NextFunction): IWebResponse 
-  {
+class Nbb {
+  get(req: Request, res: Response, next: NextFunction): IWebResponse {
     const response: IWebResponse = {
-      data:
-      {
-        message: undefined
-      }
-    }
+      data: {
+        message: undefined,
+      },
+    };
 
     return response;
   }
 
-  public post(req: any, res: any, next: NextFunction): IWebResponse
-  {
+  public post(req: any, res: any, next: NextFunction): IWebResponse {
     const response: IWebResponse = {
-      data:
-      {
-        message: `<h2>Welcome, ${(req.body as any).fname}</h2>`
-      }
-    }
+      data: {
+        message: `<h2>Welcome, ${(req.body as any).fname}</h2>`,
+      },
+    };
 
     return response;
   }
 }
-
 //#endregion
