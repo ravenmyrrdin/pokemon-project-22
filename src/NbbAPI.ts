c8004d61e30ae52235f0572a5def58ebe83b4a3f
Oste Jannick <oste.jannick@hotmail.com>
2022-03-24 20:30:57 +0100

    Keyfile added to ignore & api now using keyfile

--- src/NbbAPI.ts
+++ src/NbbAPI.ts
@@ -1,55 +1,66 @@
-/*
+
 import { AxiosInstance, AxiosResponse } from "axios"
 const axios = require("axios");
 import {v4 as uuidv4} from 'uuid';
 
-/***
- * 
- * This service provides summarized information about the annual accounts that were filed to 
- *      the Central Balance Sheet Office of the National Bank of Belgium.
+interface keydata {
+    authentic: string[];
+    'authentic-archive': string[];
+    extracts: string[];
+    'improved-archive': string[];
+    improved: string[];
+}
 
-* This service allows to retrieve the following information:
-* - The references of annual accounts filed to <u>and</u> accepted by NBB
-* - The annual account documents published to the public portal of NBB
-                        - The reproduction as PDF image by NBB of the published annual accounts
-                        - The improvements made by NBB to a selection of annual accounts
-*//*
 class NBBApi 
 {
-    private apiEndpoint: string = "https://ws.cbso.nbb.be";
+    private apiEndpoint: string = "https://ws.uat2.cbso.nbb.be";
+    private readonly keys: keydata = require("../keys.json")
 
-    public async apiRequest({endpoint = "", productKey = ""} = {}): Promise<AxiosResponse<any, any>>
+    private getCategoryKey(category: string): string | undefined
     {
-        return await new Promise<AxiosResponse<any, any>>((resolve, reject) => {
-                try
-                {
-                    resolve(axios.create().get(`${this.apiEndpoint}/${endpoint}`, {
-                        headers: {
-                            "NBB-CBSO-Subscription-Key": productKey,
-                            "X-Request-aId": uuidv4(),
-                            "Accept": "application/json"
-                    }}));
-                }
-                catch(error) 
-                {
-                    reject(error);
-                }
-            }
-        );
+        if(Object.keys(this.keys).includes(category))
+        {
+            const keySet: string = (Object.assign({}, this.keys) as any)[category];
+            if(keySet)
+                return keySet[0];
+        }
+
+        return undefined;
     }
 
-    public async getLegalEntityReferences(id: string) 
+    public get({category = "", endpoint = ""} = {}):  Promise<AxiosResponse<any, any>>
     {
-        return new Promise<AxiosResponse<any, any>>(async(resolve, reject) => await this.apiRequest({
-            endpoint: `authentic/legalEntity/${id}/references`,
-            productKey: ""
-        }));
+        return new Promise<AxiosResponse<any, any>>(async(resolve, reject) => 
+        {
+            const key: string | undefined = this.getCategoryKey(category);
+            if(!key) reject(new Error("Invalid category, allowed categories: "+Object.keys(this.keys).join(", ")))
+
+            const url: string = `${this.apiEndpoint}/${category}/${endpoint}`;
+            let response: any
+            try
+            {
+                response = await axios.create().get(url, {
+                    headers: {
+                        "NBB-CBSO-Subscription-Key": key,
+                        "X-Request-Id": uuidv4(),
+                        "Accept": "application/json"
+                }});
+            }
+            catch(error) 
+            {
+                reject(error);
+            }
+            finally
+            {
+                resolve(response);
+            }
+        });
     }
 }
 
-new NBBApi().apiRequest({
-    endpoint: `authentic/legalEntity/0203201340/references`,
-    productKey: ""
-}).then((res: AxiosResponse<any, any>) => console.log(res))
-.catch((error: Error) => console.log(error.message));
-*/
+const api = new NBBApi();
+
+api.get({
+    endpoint: "deposit/0203201340/accountingData",
+    category: "authentic"
+})
