7ad78f5f11b8a5de05c2805657b50cc55b8ff915
Oste Jannick <oste.jannick@hotmail.com>
2022-03-25 19:21:06 +0100

    Reference toegevoegd

--- src/misc/StringTools.ts
+++ src/misc/StringTools.ts
@@ -16,6 +16,7 @@ export class StringTools
 
     public static isEmail(str: string): boolean 
     {
+        // https://www.emailregex.com/
         return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(str);
     }
 }
