7bcf0cddd0a27f32bccd4764d83d9a2b3e0ef457
RavenMyrrdin <ravenvanhove@hotmail.be>
2022-04-04 12:28:50 +0200

    "Resolved popup according to wishes of the cliënt"

---
+++ src/net/views/Popup.ts
@@ -0,0 +1,17 @@
+/**
+ * @module src.net.views
+ */
+import { NextFunction } from "express";
+import { IWebResponse } from "../interfaces/IWebResponse";
+import { IViewTemplate } from "../interfaces/IViewTemplate";
+
+/**
+ * Callback interface based on IWebRequest for endpoint: /.
+ */
+export const Index: IViewTemplate = {
+  get(req: Request, res: Response, next: NextFunction): IWebResponse {
+    return { data: {} };
+  },
+
+  post: undefined,
+};
