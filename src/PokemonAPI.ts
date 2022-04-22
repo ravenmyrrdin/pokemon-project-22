3c78317291ee45142b5150a24e7e2ec6974560ad
Oste Jannick (s129362) <jannick.oste@student.ap.be>
2022-04-22 10:45:44 +0200

    Pokemon API update - generic pokemon class added

--- src/PokemonAPI.ts
+++ src/PokemonAPI.ts
@@ -1,16 +1,37 @@
 import axios from "axios";
+import { debug } from "console";
+import { Globals } from "./misc/Globals";
+import { StringTools } from "./misc/StringTools";
 
-interface Ability 
+interface IAbility 
 {
   name: string,
   url: string
 }
 
+interface ISprites {
+  back_default?: string;
+  back_female?: string;
+  back_shiny?: string;
+  back_shiny_female?: string;
+  front_default?: string;
+  front_female?: string;
+  front_shiny?: string;
+  front_shiny_female?: string;
+}
+
+
 class Pokemon 
 {
   constructor(json: any)
   {
-    console.dir(json.abilities.map((i: any) => i));
+    this._id = json.id;
+    this._name = json.name;
+    this._abilities = json.abilities.map((i: any) => i.ability);
+    this._weight = json.weight;
+    this._sprites = json.sprites.versions;
+
+    console.dir(json);
   }
 
   private _id: number = -1;
@@ -25,7 +46,29 @@ class Pokemon
     return this._name;
   }
 
-  
+  private _abilities: IAbility[] = [];
+  public get abilities(): IAbility[]
+  {
+    return this._abilities;
+  }
+
+  private _baseExperience: number = -1;
+  public get baseExperience(): number
+  {
+    return this._baseExperience;
+  }
+
+  private _weight: number = -1;
+  public get weight(): number
+  {
+    return this._weight;
+  }
+
+  private _sprites: any;
+  public getSprites(generation: number)
+  {
+    return this._sprites[`generation-${StringTools.numberToRoman(generation)}`];
+  }
 }
 
 class PokemonAPI 
@@ -44,4 +87,11 @@ class PokemonAPI
     }
 }
 
-new PokemonAPI().getByName("ditto");
+new PokemonAPI().getByName("ditto").then(p => {
+  console.log(`ID: ${p.id}`);
+  console.log(`Name: ${p.name}`);
+  console.log(`Weight: ${p.weight}`);
+  console.log(`Abilities: ${JSON.stringify(p.weight)}`);
+  console.log(`Sprites (GEN 1): ${JSON.stringify(p.getSprites(1))}`);
+
+});
