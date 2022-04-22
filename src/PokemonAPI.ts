c24a1befb4175a09ddd816eb423895076eea6014
Oste Jannick (s129362) <jannick.oste@student.ap.be>
2022-04-22 09:13:04 +0200

    Heroku test

--- src/PokemonAPI.ts
+++ src/PokemonAPI.ts
@@ -1,10 +1,41 @@
 import axios from "axios";
 
+interface Ability 
+{
+  name: string,
+  url: string
+}
+
+class Pokemon 
+{
+  constructor(json: any)
+  {
+    console.dir(json.abilities.map((i: any) => i));
+  }
+
+  private _id: number = -1;
+  public get id(): number
+  {
+    return this._id;
+  }
+
+  private _name: string = "";
+  public get name(): string
+  {
+    return this._name;
+  }
+
+  
+}
+
 class PokemonAPI 
 {
-    public async getByName(name: string) 
+    public async getByName(name: string): Promise<Pokemon>
     {
-      return (await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)).data;
+      const apiData =  (await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)).data;
+      const pokemon: Pokemon = new Pokemon(apiData);
+
+      return pokemon;
     }
     
     public async getById(id: number)
@@ -13,4 +44,4 @@ class PokemonAPI
     }
 }
 
-new PokemonAPI().getByName("ditto").then(i => console.log(i));
+new PokemonAPI().getByName("ditto");
