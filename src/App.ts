3e996b6a1ffaecccda8d965381188b393d6e4383
Artur Kovalenko <kovalenko_artur@msn.com>
2022-05-06 09:45:54 +0200

    fixed popu

--- src/App.ts
+++ src/App.ts
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.set("port", 8080 || process.env.PORT);
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static("public"));  
 
 app.get("/", (req: any, res: any) => {
   res.render("index");
@@ -16,6 +16,9 @@ app.get("/catch", (req: any, res: any) => {
   res.render("catch");
 });
 
+app.get("/popup", (req: any, res: any) => {
+  res.render("popup");
+});
 app.listen(
   app.get("port"),
   console.log(`[SERVER]: Running on http://localhost:${app.get("port")}`)
