744de50a11e31d26fe158df13b52e7b8dad43a4e
Oste Jannick (s129362) <jannick.oste@student.ap.be>
2022-05-06 10:51:12 +0200

    Layout update

--- src/App.ts
+++ src/App.ts
@@ -26,34 +26,14 @@ app.get("/vergelijking", async (req: any, res: any) => {
     const pokemonB = await api.getById(2);
 
     res.render("vergelijking", { 
-        "sprite": [
-            pokemonA.getFrontSprite(PokemonGame.RedBlue),
-            pokemonB.getFrontSprite(PokemonGame.RedBlue)
-        ],
-        "attack": [
-            pokemonA.getStat(IPokemonStat.Attack),
-            pokemonB.getStat(IPokemonStat.Attack)
-        ],
-        "hp": [
-            pokemonA.getStat(IPokemonStat.HP),
-            pokemonB.getStat(IPokemonStat.HP)
-        ],
-        "defence": [
-            pokemonA.getStat(IPokemonStat.Defence),
-            pokemonB.getStat(IPokemonStat.Defence)
-        ],
-        "specialattack": [
-            pokemonA.getStat(IPokemonStat.SpecialAttack),
-            pokemonB.getStat(IPokemonStat.SpecialAttack)
-        ],
-        "specialdefence": [
-            pokemonA.getStat(IPokemonStat.SpecialDefence),
-            pokemonB.getStat(IPokemonStat.SpecialDefence)
-        ],
-        "speed": [
-            pokemonA.getStat(IPokemonStat.SpecialDefence),
-            pokemonB.getStat(IPokemonStat.SpecialDefence)
-        ]
+        "name":           [ pokemonA.name, pokemonB.name ],
+        "sprite":         [ pokemonA.getFrontSprite(PokemonGame.RedBlue), pokemonB.getFrontSprite(PokemonGame.RedBlue) ],
+        "attack":         [ pokemonA.getStat(IPokemonStat.Attack), pokemonB.getStat(IPokemonStat.Attack) ],
+        "hp":             [ pokemonA.getStat(IPokemonStat.HP), pokemonB.getStat(IPokemonStat.HP) ],
+        "defence":        [ pokemonA.getStat(IPokemonStat.Defence), pokemonB.getStat(IPokemonStat.Defence) ],
+        "specialattack":  [ pokemonA.getStat(IPokemonStat.SpecialAttack), pokemonB.getStat(IPokemonStat.SpecialAttack) ],
+        "specialdefence": [ pokemonA.getStat(IPokemonStat.SpecialDefence), pokemonB.getStat(IPokemonStat.SpecialDefence) ],
+        "speed":          [ pokemonA.getStat(IPokemonStat.SpecialDefence), pokemonB.getStat(IPokemonStat.SpecialDefence) ]
     });
 });
 
