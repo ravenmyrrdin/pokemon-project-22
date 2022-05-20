4b5017f89c31039c74599d8842fa4fc284d881c7
Van Dijck Alex (s131627) <alex.vandijck@student.ap.be>
2022-05-20 08:50:23 +0200

    pre-merge commit

--- src/App.ts
+++ src/App.ts
@@ -14,15 +14,21 @@ app.get("/", (req: any, res: any) => {
   res.render("index");
 });
 
-app.get("/pokemon", async (req: any, res: any) => {
-  const pokemons:Pokemon[] =[];
-  for(let i =1;i<25;i++){
-    const pokemon = await api.getById(i);
-    console.log(pokemon.id + " " + pokemon.name);
-    pokemons.push(pokemon);
+app.get("/pokemon", async (req: any, res: any) => res.redirect("/pokemon/0"));
+app.get("/pokemon/:page", async (req: any, res: any) => {
+  if(isNaN(Number.parseInt(req.params.page)))
+    return res.redirect("/pokemon/0");
+
+  const pokemonFetchers:Promise<Pokemon>[] =[];
+  let page = Number.parseInt(req.params.page);
+  let itemsOnPage = 30;
+  for(let i =(itemsOnPage*page)+1; i<(itemsOnPage*(page+1))+1;i++){
+    const pokemon = api.getById(i);
+    pokemonFetchers.push(pokemon);
   }
-  res.render("pokemon", {
-    pokemons: await pokemons,
+  res.render("pokemon",{
+    "pokemons": await Promise.all(pokemonFetchers),
+    "pageId": page
   });
 });
 
@@ -31,17 +37,9 @@ app.get("/pokemon", async (req: any, res: any) => {
 // });
 
 app.get("/catch/:index", async (req: any, res: any) => {
-  const index = req.params.index;
-  // if (index > 0 && index != null) {
-  //   const pokemon: Pokemon = await api.getById(index);
-  //   res.render("catching", { pokemon: await pokemon });
-  // } else {
-  //   res.render("404")
-  // }
-  const pokemon: Pokemon = await api.getById(index);
-  try {
-  res.render("catching", { pokemon: await pokemon });
-  } catch (err) {console.error(err);}
+  const pokemon:Pokemon = await api.getById(req.param);
+  res.render("catching", {pokemon : pokemon});
+
 });
 
 app.get("/dashboard", (req: any, res: any) => {
