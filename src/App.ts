89b1934de04e2b94bc0632680a3170178a34500e
RavenMyrrdin <ravenvanhove@hotmail.be>
2022-06-02 19:22:26 +0200

    Update

--- src/App.ts
+++ src/App.ts
@@ -1,56 +1,57 @@
-import { PokemonGame } from './api/PokemonGame';
+import { PokemonGame } from "./api/PokemonGame";
 import { IPokemonStat } from "./api/IPokemonStat";
 import { Pokemon } from "./api/Pokemon";
 import { PokemonAPI } from "./api/PokemonAPI";
 
-const checkSessionToken = (req, res, next) =>
-{
+const checkSessionToken = (req, res, next) => {
   var cookie = req.cookies.cookieName;
-  if(cookie === undefined)
-  {
+  if (cookie === undefined) {
     let days = 365;
-    res.cookie('sessionToken', `${Math.random()}`.slice(2), { maxAge: (24*60*60*1000)*days, httpOnly: true });
+    res.cookie("sessionToken", `${Math.random()}`.slice(2), {
+      maxAge: 24 * 60 * 60 * 1000 * days,
+      httpOnly: true,
+    });
   }
 
   next();
-}
+};
 
 const express = require("express");
 const app = express();
 const api = new PokemonAPI();
-var cookieParser = require('cookie-parser')
+var cookieParser = require("cookie-parser");
 
 app.set("port", process.env.PORT || 8080);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(checkSessionToken);
 
 app.get("/", (req: any, res: any) => {
   // Cookies that have not been signed
-  console.dir(req.cookies)
+  console.dir(req.cookies);
 
   // Cookies that have been signed
-  console.log('Signed Cookies: ', req.signedCookies)
+  console.log("Signed Cookies: ", req.signedCookies);
   res.render("index");
 });
 
 app.get("/pokemon", async (req: any, res: any) => res.redirect("/pokemon/0"));
 app.get("/pokemon/:page", async (req: any, res: any) => {
-  if(isNaN(Number.parseInt(req.params.page)))
+  if (isNaN(Number.parseInt(req.params.page)))
     return res.redirect("/pokemon/0");
-  
-  const pokemonFetchers:Promise<Pokemon>[] =[];
+
+  const pokemonFetchers: Promise<Pokemon>[] = [];
   let page = Number.parseInt(req.params.page);
   let itemsOnPage = 30;
-  for(let i =(itemsOnPage*page)+1; i<(itemsOnPage*(page+1))+1;i++){
+  for (let i = itemsOnPage * page + 1; i < itemsOnPage * (page + 1) + 1; i++) {
     const pokemon = api.getById(i);
     pokemonFetchers.push(pokemon);
   }
-  res.render("pokemon",{
-    "pokemons": await Promise.all(pokemonFetchers),
-    "pageId": page
+  res.render("pokemon", {
+    pokemons: await Promise.all(pokemonFetchers),
+    pageId: page,
   });
 });
 
@@ -68,72 +69,90 @@ app.get("/catch/:index", async (req: any, res: any) => {
   // }
   const pokemon: Pokemon = await api.getById(index);
   try {
-  res.render("catching", { pokemon: await pokemon });
-  } catch (err) {console.error(err);}
+    res.render("catching", { pokemon: await pokemon });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 app.get("/dashboard", (req: any, res: any) => {
-
   res.render("dashboard");
 });
 
-app.get("/vergelijking", async (req: any, res: any) => res.redirect("/vergelijking/1/2"));
-app.post("/vergelijking/:a/:b", async (req: any, res: any) =>  res.redirect(`/vergelijking/${req.body.aIdentifier}/${req.body.bIdentifier}`));
+app.get("/vergelijking", async (req: any, res: any) =>
+  res.redirect("/vergelijking/1/2")
+);
+app.post("/vergelijking/:a/:b", async (req: any, res: any) =>
+  res.redirect(`/vergelijking/${req.body.aIdentifier}/${req.body.bIdentifier}`)
+);
 app.get("/vergelijking/:a/:b", async (req: any, res: any) => {
-    // Catch promise rejections using map -> p.catch aka then equivalent, and map results to undefined if reject or value if resolved.
-    const [pokemonA, pokemonB] = (await Promise.all([ 
-      /^[0-9]+$/.test(req.params.a) ? api.getById(Number.parseInt(req.params.a)) : api.getByName(req.params.a),
-      /^[0-9]+$/.test(req.params.b) ? api.getById(Number.parseInt(req.params.b)) : api.getByName(req.params.b)
-    ].map(p => p.catch(e => e)))).map(i => i instanceof Error ? undefined : i);
+  // Catch promise rejections using map -> p.catch aka then equivalent, and map results to undefined if reject or value if resolved.
+  const [pokemonA, pokemonB] = (
+    await Promise.all(
+      [
+        /^[0-9]+$/.test(req.params.a)
+          ? api.getById(Number.parseInt(req.params.a))
+          : api.getByName(req.params.a),
+        /^[0-9]+$/.test(req.params.b)
+          ? api.getById(Number.parseInt(req.params.b))
+          : api.getByName(req.params.b),
+      ].map((p) => p.catch((e) => e))
+    )
+  ).map((i) => (i instanceof Error ? undefined : i));
 
-    const responseData = 
-    { 
-      "infoA": pokemonA === undefined ? undefined : {
-        "name": pokemonA.name,
-        "sprite": pokemonA.getFrontSprite(PokemonGame.DiamondPearl),
-        "stats": 
-        {
-          "attack":         pokemonA.getStat(IPokemonStat.Attack),        
-          "hp":             pokemonA.getStat(IPokemonStat.HP),            
-          "defence":        pokemonA.getStat(IPokemonStat.Defence),       
-          "specialattack":  pokemonA.getStat(IPokemonStat.SpecialAttack), 
-          "specialdefence": pokemonA.getStat(IPokemonStat.SpecialDefence),
-          "speed":          pokemonA.getStat(IPokemonStat.SpecialDefence),
-        }
-      },
-      "infoB": pokemonB === undefined ? undefined : {
-        "name": pokemonB.name,
-        "sprite": pokemonB.getFrontSprite(PokemonGame.DiamondPearl),
-        "stats": 
-        {
-          "attack":         pokemonB.getStat(IPokemonStat.Attack),        
-          "hp":             pokemonB.getStat(IPokemonStat.HP),            
-          "defence":        pokemonB.getStat(IPokemonStat.Defence),       
-          "specialattack":  pokemonB.getStat(IPokemonStat.SpecialAttack), 
-          "specialdefence": pokemonB.getStat(IPokemonStat.SpecialDefence),
-          "speed":          pokemonB.getStat(IPokemonStat.SpecialDefence),
-        }
-      }
-    };
-    
-    // Count superior stats.
-    let aCount = responseData.infoA === undefined ? -1 : 0;
-    let bCount = responseData.infoB === undefined ? -1 : 0;
-    if(aCount !== -1 && bCount !== -1)
-    {
-      for(let [key, value] of Object.entries(responseData.infoA))
-      {
-        if(value < responseData.infoB[key])
-          bCount++;
-        else aCount++;
-      }
-    }
+  const responseData = {
+    infoA:
+      pokemonA === undefined
+        ? undefined
+        : {
+            name: pokemonA.name,
+            sprite: pokemonA.getFrontSprite(PokemonGame.DiamondPearl),
+            stats: {
+              attack: pokemonA.getStat(IPokemonStat.Attack),
+              hp: pokemonA.getStat(IPokemonStat.HP),
+              defence: pokemonA.getStat(IPokemonStat.Defence),
+              specialattack: pokemonA.getStat(IPokemonStat.SpecialAttack),
+              specialdefence: pokemonA.getStat(IPokemonStat.SpecialDefence),
+              speed: pokemonA.getStat(IPokemonStat.SpecialDefence),
+            },
+          },
+    infoB:
+      pokemonB === undefined
+        ? undefined
+        : {
+            name: pokemonB.name,
+            sprite: pokemonB.getFrontSprite(PokemonGame.DiamondPearl),
+            stats: {
+              attack: pokemonB.getStat(IPokemonStat.Attack),
+              hp: pokemonB.getStat(IPokemonStat.HP),
+              defence: pokemonB.getStat(IPokemonStat.Defence),
+              specialattack: pokemonB.getStat(IPokemonStat.SpecialAttack),
+              specialdefence: pokemonB.getStat(IPokemonStat.SpecialDefence),
+              speed: pokemonB.getStat(IPokemonStat.SpecialDefence),
+            },
+          },
+  };
 
-    res.render("vergelijking", {
-      ...responseData, 
-      "superior": aCount > bCount ? 0 : bCount > aCount ? 1 : -1
+  // Count superior stats.
+  let aCount = responseData.infoA === undefined ? -1 : 0;
+  let bCount = responseData.infoB === undefined ? -1 : 0;
+  if (aCount !== -1 && bCount !== -1) {
+    for (let [key, value] of Object.entries(responseData.infoA)) {
+      if (value < responseData.infoB[key]) bCount++;
+      else aCount++;
     }
-    );
+  }
+
+  res.render("vergelijking", {
+    ...responseData,
+    superior: aCount > bCount ? 0 : bCount > aCount ? 1 : -1,
+  });
+});
+
+app.get("/pokemon-detail/:id", async (req: any, res: any) => {
+  let data = await api.getById(req.params.id);
+  console.log(data)
+  res.render("singlePokemon", { data: data });
 });
 
 app.listen(
