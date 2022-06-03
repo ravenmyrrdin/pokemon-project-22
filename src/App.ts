42dcdaa14a2c79b8e4362d9910cef423f0ffa0b7
RavenMyrrdin <ravenvanhove@hotmail.be>
2022-06-03 08:59:56 +0200

    Update

--- src/App.ts
+++ src/App.ts
@@ -194,7 +194,7 @@ app.get("/vergelijking/:a/:b", async (req: any, res: any) => {
 
 app.get("/pokemon-detail/:id", async (req: any, res: any) => {
   let data = await api.getById(req.params.id);
-  let database = true;
+  let database = req.user?.capturedPokemon;
   console.log(data)
   res.render("singlePokemon", { data: data, database: database, releasePokemon: releasePokemon });
 });
