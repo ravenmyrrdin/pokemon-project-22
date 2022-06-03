e89a23df410ed48ab4e98ee6724deeceae8ccb86
Oste Jannick (s129362) <jannick.oste@student.ap.be>
2022-06-03 04:49:46 +0200

    db testing

--- src/Database.ts
+++ src/Database.ts
@@ -8,11 +8,56 @@ let pikachu:Pokemon={
     name:"pikachu"
 }
 
-interface Pokemon{
+export interface Pokemon{
     id?:number,
     name:string
 }
-let doSomeDBCalls = async () => {
+
+let getUser = async(sessionToken: string) => 
+{
+    let output;
+    try
+    {
+        await client.connect();
+        const repo = await client.db("ITProject");
+        const collection = await repo.collection("pokemons")
+
+        output = await collection.findOne({sessionToken: sessionToken});
+    }
+    catch
+    {
+
+    }
+    finally
+    {
+        client.close();
+        return output;
+    }
+}
+
+let setUser = async(sessionToken: IUser) => 
+{
+    let output;
+    try
+    {
+        await client.connect();
+        const repo = await client.db("ITProject");
+        const collection = await repo.collection("pokemons")
+
+        output = await collection.findOne({sessionToken: sessionToken});
+    }
+    catch
+    {
+
+    }
+    finally
+    {
+        client.close();
+        return output;
+    }
+}
+
+let insertUser = async (pokemon) => {
     try {
         // Connect to the MongoDB cluster
         await client.connect();
@@ -29,7 +74,6 @@ let doSomeDBCalls = async () => {
     }
 }
 console.log(pikachu);
-doSomeDBCalls();
 
-
+console.dir(getUser(""));
 
